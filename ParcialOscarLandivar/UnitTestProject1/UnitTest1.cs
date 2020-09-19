e59e646c1b72d18788aaffb35ca8aa646c923271
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-09-19 13:07:20 -0400

    proyecto terminado

---
+++ ParcialOscarLandivar/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialOscarLandivar.Controllers;
+
+namespace UnitTestPregunta1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGet()
+        {
+            //Arrange
+            PaisesController paisesController = new PaisesController();
+
+            //Act
+            var Resultado = paisesController.GetPaises();
+
+            //Assert
+            Assert.IsNotNull(Resultado);
+
+        }
+    }
+}
